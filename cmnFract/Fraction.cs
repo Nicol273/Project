4867e7f775641a372d2fe61416a4f174ae3dc356
Nicol <nicol.dobreva12@gmail.com>
2021-05-19 20:30:56 +0300

    Initial commit

---
+++ cmnFract/Fraction.cs
@@ -0,0 +1,131 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Nicol
+ * Date: 25.3.2021 г.
+ * Time: 8:19
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace cmnFract
+{
+	public class Fraction
+	{
+		public int num, denom;
+		public Fraction()
+		{
+			num = 0;
+			denom = 1;
+		}
+		public Fraction(int n)
+		{
+			num = n;
+			denom = 1;
+		}
+		public Fraction(int n, int d)
+		{
+			num = n;
+			if(d == 0) throw new ArgumentException("Забранени неща!");
+			denom = d;
+			if(d < 0)
+			{
+				denom = (-1) * d;
+				num *= (-1);
+			}
+		}
+		public Fraction(Fraction other)
+		{
+			num = other.num;
+			denom = other.denom;
+		}
+		private static int GCD(int a, int b)
+		{
+			int q;
+            while (b != 0)
+            {
+                q = b;
+                b = a % b;
+                a = q;
+            }
+            return a;
+		}
+		public void Add(Fraction other)
+		{
+			int r;
+			num = num * other.denom + other.num * denom;
+			denom *= other.denom;
+			r = GCD(num, denom);
+			if(r > 1)
+			{
+				num /= r;
+				denom /= r;
+			}
+		}
+		public void Sub(Fraction other)
+		{
+			int r;
+			num = num * other.denom - other.num * denom;
+			denom *= other.denom;
+			r = GCD(num, denom);
+			if(r > 1)
+			{
+				num /= r;
+				denom /= r;
+			}
+		}
+		public void Mult(Fraction other)
+		{
+			int r;
+			num *= other.num;
+			denom *= other.denom;
+			r = GCD(num, denom);
+			if(r > 1)
+			{
+				num /= r;
+				denom /= r;
+			}
+		}
+		public void Div(Fraction other)
+		{
+			int r;
+			num *= other.denom;
+			denom *= other.num;
+			r = GCD(num, denom);
+			if(r > 1)
+			{
+				num /= r;
+				denom /= r;
+			}
+		}
+		public override string ToString()
+		{
+			if(denom == 1) return string.Format("{0}", num);
+			return string.Format("{0}/{1}", num, denom);
+		}
+		public static Fraction operator + (Fraction f1, Fraction f2)
+		{
+			Fraction f = new Fraction(f1);
+			f.Add(f2);
+			return f;
+		}
+		public static Fraction operator - (Fraction f1, Fraction f2)
+		{
+			Fraction f = new Fraction(f1);
+			f.Sub(f2);
+			return f;
+		}
+		public static Fraction operator * (Fraction f1, Fraction f2)
+		{
+			Fraction f = new Fraction(f1);
+			f.Mult(f2);
+			return f;
+		}
+		public static Fraction operator / (Fraction f1, Fraction f2)
+		{
+			Fraction f = new Fraction(f1);
+			f.Div(f2);
+			return f;
+		}
+	}
+}
---
+++ cmnFract/Program.cs
@@ -0,0 +1,61 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Nicol
+ * Date: 25.3.2021 г.
+ * Time: 8:13
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace cmnFract
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Въведете първата дроб");
+			Console.Write("Числител: ");
+			int a = int.Parse(Console.ReadLine());
+			Console.Write("Знаменател: ");
+			int b = int.Parse(Console.ReadLine());
+			Console.WriteLine("Въведете втората дроб");
+			Console.Write("Числител: ");
+			int c = int.Parse(Console.ReadLine());
+			Console.Write("Знаменател: ");
+			int d = int.Parse(Console.ReadLine());
+			Fraction f1 = new Fraction(a, b);
+			Fraction f2 = new Fraction(c, d);
+			Console.WriteLine("{0} + {1} = {2}", f1, f2, f1 + f2);
+			Console.WriteLine("{0} - {1} = {2}", f1, f2, f1 - f2);
+			Console.WriteLine("{0} . {1} = {2}", f1, f2, f1 * f2);
+			Console.WriteLine("{0} : {1} = {2}", f1, f2, f1 / f2);
+			Console.ReadKey(true);
+			Console.WriteLine("Въведете естествено числа A и B (A < B)");
+			Console.Write("A: ");
+			int A = int.Parse(Console.ReadLine());
+			Console.Write("B: ");
+			int B = int.Parse(Console.ReadLine());
+			Fraction[] allFracts = new Fraction[B-A+1];
+			int m = A;
+			for(int i = 0; i < B-A+1; i++)
+			{
+				allFracts[i] = new Fraction(1, m);
+				m++;
+			}
+			/*foreach(Fraction f in allFracts)
+			{
+				Console.WriteLine(f);
+			}*/
+			Fraction subTotal = new Fraction();
+			for(int i = 0; i < allFracts.Length; i++)
+			{
+				subTotal = new Fraction(subTotal + allFracts[i]);
+				Console.Write(allFracts[i]);
+				if(i < allFracts.Length - 1) Console.Write(" + ");
+			}
+			Console.WriteLine(" = " + subTotal);
+			Console.ReadKey(true);
+		}
+	}
+}
